aee8374cc42a487fc95b35771e219755f71c5286
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-02 17:40:48 +0700

    use nodemon

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -2,6 +2,6 @@ import type { Request, Response } from "express";
 
 export const HealthCheck = (req: Request, res: Response): void => {
   res.status(200).json({
-    message: "OK",
+    message: "OK again",
   });
 };
