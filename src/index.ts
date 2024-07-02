cc81ca7928260eeb56973a7a6cdd16a2a0eae07a
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-02 17:47:42 +0700

    add handler when api not found

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const port = 8080;
 
 app.use("/", mainRoute);
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: "Not Found",
+    message: "Hacker?",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
