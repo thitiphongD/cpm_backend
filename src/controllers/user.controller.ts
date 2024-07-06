d09ff3bad4cb2d250b0ae0d503e56960d5383c97
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-06 17:20:34 +0700

    add get coin api

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express";
 import pool from '../db/connection'
 import dotenv from "dotenv";
 dotenv.config();
+const apiKey = process.env.CMC_API_KEY;
 
 export const coinMarketCapAPI = async (req: Request, res: Response) => {
     const apiUrl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
-    const apiKey = process.env.CMC_API_KEY;
 
     if (!apiKey) {
         console.error('API key not found in env');
@@ -31,6 +31,36 @@ export const coinMarketCapAPI = async (req: Request, res: Response) => {
         res.status(500).json({ error: 'Failed to fetch data from CoinMarketCap API' });
     }
 };
+
+export const getCoin = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    console.log('Requested cryptocurrency ID:', id);
+
+    const apiUrl = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/info';
+
+    if (!id) {
+        console.error('API key not found in env');
+        return res.status(500).json({ error: 'coin not found' }); 
+    }
+
+    if (!apiKey) {
+        console.error('API key not found in env');
+        return res.status(500).json({ error: 'API key not found' });
+    }
+    const url = `${apiUrl}?id=${id}`;
+    const response = await fetch(url, {
+        headers: {
+            'X-CMC_PRO_API_KEY': apiKey,
+            'Accept': 'application/json'
+        }
+    });
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    res.json(data);
+}
 
 export const login = async (req: Request, res: Response): Promise<void> => {
     const { username, password } = req.body;
--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,12 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { coinMarketCapAPI, getAllUsers, getPortfolio, login, register } from '../controllers/user.controller'
+import { coinMarketCapAPI, getAllUsers, getCoin, getPortfolio, login, register } from '../controllers/user.controller'
 
 router.get("/users", getAllUsers);
 router.post("/login", login);
 router.post("/register", register);
 router.get("/portfolio", getPortfolio)
 router.get("/coin-list", coinMarketCapAPI)
-
+router.get("/coins/:id", getCoin)
 
 export default router;
