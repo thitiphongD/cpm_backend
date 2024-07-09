1c7df1810ba3a4206ca1c3ed4383a2c7d96987ae
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-09 21:54:00 +0700

    Delete config.ts

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-const HOST = process.env.HOST;
-const SERVER_PORT = process.env.SERVER_PORT;
-
-export const BASE_URL: string = `${HOST}:${SERVER_PORT}`;
