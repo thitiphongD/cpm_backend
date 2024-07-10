b38d22525ca67ec434c9da3dae1ac6c6403360a0
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-10 12:04:04 +0700

    Delete main.ts

--- src/controllers/main.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from "express";
-
-export const HealthCheck = (req: Request, res: Response): void => {
-  res.status(200).json({
-    message: "OK again",
-  });
-};
