bb217e8542be2e7b4161ec96de034885da9063ee
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-10 21:49:42 +0700

    refactor code

---
+++ src/helpers/portfolioHelpers.ts
@@ -0,0 +1,33 @@
+import { PortfolioData } from "../interface/interface";
+import { fetchCoinData } from "../models/coin";
+import { fetchPortfolioData } from "../models/user";
+
+export const mergePortfolioData = (portfolioRows: any, resultCoins: any): PortfolioData[] => {
+    return portfolioRows.map((row: any) => {
+        const coinData = resultCoins.data[row.crypto_id];
+        const price = resultCoins.data[row.crypto_id].quote["USD"].price;
+        const amount = parseFloat(row.quantity) * price;
+
+        return {
+            id: row.crypto_id,
+            name: coinData.name,
+            symbol: coinData.symbol,
+            cmc_rank: coinData.cmc_rank,
+            price: price,
+            percent_change_1h: coinData.quote["USD"].percent_change_1h,
+            percent_change_24h: coinData.quote["USD"].percent_change_24h,
+            percent_change_7d: coinData.quote["USD"].percent_change_7d,
+            volume_24h: coinData.quote["USD"].volume_24h,
+            market_cap: coinData.quote["USD"].market_cap,
+            quantity: parseFloat(row.quantity),
+            amount: amount,
+        };
+    });
+}
+
+export const getPortfolioAndCoinData = async (username: string) => {
+    const portfolioRows = await fetchPortfolioData(username);
+    const cryptoIds = portfolioRows.map((row) => row.crypto_id).join(",");
+    const resultCoins = await fetchCoinData(cryptoIds);
+    return { portfolioRows, resultCoins };
+}
