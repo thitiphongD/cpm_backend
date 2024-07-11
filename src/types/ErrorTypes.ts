718b0079452576787706b65fcf7f3ddec08e2799
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-11 21:30:43 +0700

    fix auth provider

--- src/types/ErrorTypes.ts
+++ src/types/ErrorTypes.ts
@@ -1,6 +1,6 @@
 export enum ErrorType {
   SERVER_ERROR = "Server error",
-  LOGIN_FAIL = "Login Fail",
+  LOGIN_FAIL = "Username or Password Incorrect",
   PASSWORD_NOT_MATCH = "Passwords do not match!",
   REGISTER_FAIL = "Failed to register user",
   USER_EXISTS = "User already exists",
@@ -16,5 +16,5 @@ export enum SuccessType {
   LOGIN = "Login success",
   COIN_SUCCESS = "Add coin success",
   PORT_SUCCESS = "Get Portfolio success",
-  UPDATE_PORT_SUCCESS = "Portfolio update success"
+  UPDATE_PORT_SUCCESS = "Portfolio update success",
 }
